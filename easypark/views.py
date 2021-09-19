d1b99d5584b5e3d14e6fd3cbf4da0596316d000c
Luc Perin Panta <lucapameni@gmail.com>
2021-09-19 18:10:03 +0100

    derniere modif

--- easypark/views.py
+++ easypark/views.py
@@ -1,3 +1,22 @@
+from easypark.serializers import DataSerializer, NetworkSerializer, PlaceSerializer
+from easypark.models import Data, Network, Place
+from typing import Generic
 from django.shortcuts import render
-
+from rest_framework import generics,permissions, serializers ,viewsets,status
 # Create your views here.
+
+class AdminNetworkCreate(generics.CreateAPIView):
+    queryset=Network.objects.all()
+    serializer_class=NetworkSerializer
+class DataCreate(generics.CreateAPIView):
+    queryset=Data.objects.all()
+    serializer_class=DataSerializer
+class AdminNodeCreate(generics.CreateAPIView):
+    queryset=Data.objects.all()
+    serializer_class=DataSerializer
+class Reserver(generics.CreateAPIView):
+    queryset=Place.objects.all()
+    serializer_class=PlaceSerializer
+
+
+
