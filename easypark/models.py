85ff15162a7c201ebc2d7b436d531d3b864f81e2
joan30122001 <ngounouloic675@gmail.com>
2021-09-22 11:24:20 +0200

    Le user et les CRUD de l'application

---
+++ easypark/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from users.models import User
+# Create your models here.
+class Network(models.Model):
+    nameNet=models.CharField(max_length=50)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    adress=models.CharField(max_length=50)
+
+    def __str__(self) :
+        return self.nameNet
+
+class Node(models.Model):
+    name=models.CharField(max_length=50)
+    description=models.CharField(max_length=300)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    network=models.ForeignKey(Network,on_delete=models.SET_NULL,null=True)
+
+    def __str__(self):
+        return self.name
+        
+class Data(models.Model):
+    data=models.FloatField()
+    node=models.ForeignKey(Node ,on_delete=models.SET_NULL,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+class Place(models.Model):
+    name=models.CharField(max_length=50)
+    network=models.ForeignKey(Network,on_delete=models.SET_NULL,null=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
---
+++ easypark/serializers.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Data, Network, Node, Place
+
+class NetworkSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields=('id','user','nameNet','created_at' ,'updated_at','adress')
+        model=Network
+    
+class NodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields=('id','name', 'description','created_at' ,'updated_at','network')
+        model =Node
+
+class DataSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields=('id','data','node' ,'created_at')
+        model=Data
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields=('id' ,'network' ,'user')
+        model=Place
