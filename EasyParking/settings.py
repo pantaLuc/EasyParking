17816a17a91c6ea3ef55eb35c161bfc961b4f0cf
Luc Perin Panta <lucapameni@gmail.com>
2021-09-25 21:00:30 +0100

    test3

--- EasyParking/settings.py
+++ EasyParking/settings.py
@@ -58,6 +58,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
