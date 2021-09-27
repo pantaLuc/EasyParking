6a947734660a8804c3e430280a79f880dbd9ce22
Luc Perin Panta <lucapameni@gmail.com>
2021-09-27 12:17:05 +0100

    solving yesterday problem

--- easypark/admin.py
+++ easypark/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Place ,Network ,Node ,Data
+admin.site.register(Place)
+admin.site.register(Network)
+admin.site.register(Node)
+admin.site.register(Data)
 # Register your models here.
